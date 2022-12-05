1016aab31974ff6fe89b1e9134ed280dd9bb32af
artqs01 <kus.artur78@gmail.com>
2022-12-05 12:01:47 +0100

    some changes in comments

--- src/adc_control.c
+++ src/adc_control.c
@@ -4,19 +4,17 @@
 
 void adc_enable()
 {
-	//Enable ADC
 	ADCSRA = (1 << ADEN);
 }
 
 void adc_disable()
 {
-	//Disable ADC
 	ADCSRA &= ~(1 << ADEN);
 }
 
 uint16_t adc_read(uint8_t mux)
 {
-	//Read data from selected ADC (VCC as reference volatge)
+	//Read data from selected ADC with VCC as reference voltage
 	ADMUX = (mux << MUX0) | (1 << REFS0) | (1 << ADLAR);
 	ADCSRA |= (1 << ADSC);
 	while (ADCSRA & (1 << ADSC));
